58e4588ab1c4bf9f5ff63f24385f461ea89a3257
limavfabio <92121463+limavfabio@users.noreply.github.com>
2023-06-05 14:28:40 -0300

    Add models validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,9 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable, :confirmable
+
+  validates_presence_of :email, :password, :password_confirmation
+  validates_uniqueness_of :email
+  validates_confirmation_of :password
+  validates_length_of :password, minimum: 6, maximum: 20
 end
