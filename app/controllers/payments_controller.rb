1017ba0a697d15533b86fac3055aab1ad5b11941
limavfabio <92121463+limavfabio@users.noreply.github.com>
2023-06-10 09:28:18 -0300

    Creating a payment redirects to payments_path

--- app/controllers/payments_controller.rb
+++ app/controllers/payments_controller.rb
@@ -24,7 +24,7 @@ class PaymentsController < ApplicationController
 
     respond_to do |format|
       if @payment.save
-        format.html { redirect_to payment_url(@payment), notice: 'Payment was successfully created.' }
+        format.html { redirect_to payments_path, notice: 'Payment was successfully created.' }
         format.json { render :show, status: :created, location: @payment }
       else
         format.html { render :new, status: :unprocessable_entity }
